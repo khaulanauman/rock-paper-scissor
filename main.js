38c8f6305954786b500e785d1984e2dd0a0325a5
khaulanauman <khaulanauman@gmail.com>
2024-02-24 12:23:57 +0500

    added logic for playTurn function

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict"
+//user choice
+//computer choice
+//play function
+//take input from user and compter 
+//play-for one baari
+//game function will play it 3 times keeping tract of winner in each turn through counter
+//when loop ends result is displayed
+let userChoice;
+compyterC;
+console.log(compyterC);
